301bc12c58bc5938c4e61e03c152395518980641
mphokgooa <mphokgooa53@gmail.com>
2025-11-16 12:52:45 +0200

    Delete $tickynotes directory

--- $tickynotes/stickynotes/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Note
-
-@admin.register(Note)
-class NoteAdmin(admin.ModelAdmin):
-    list_display = ('title', 'owner', 'created_at')
-
-    list_filter = ('owner', 'created_at')
-    search_fields = ('title', 'content')
